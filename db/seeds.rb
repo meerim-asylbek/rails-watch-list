4b44d4a332ca001d45deec82d2e213764ebdd0b7
Meerim Asylbekova <meerim.asylbekova@gmail.com>
2022-10-15 17:18:36 +0200

    Add models and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# require 'rest-client'
+
+# response = RestClient.get "http://tmdb.lewagon.com/movie/top_rated"
+# movies = JSON.parse(response)
+
+require 'open-uri'
+require 'json'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+movies = JSON.parse(URI.open(url).read)
+
+movies['results'].each do |movie|
+  Movie.create(title: movie['original_title'], overview: movie['overview'], rating: movie['vote_average'], poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}")
+end
