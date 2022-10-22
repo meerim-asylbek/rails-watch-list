2b52d25689b984e53fad0571ce437004a6599b2a
Meerim Asylbekova <meerim.asylbekova@gmail.com>
2022-10-22 10:04:19 +0200

    Add css to pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,5 @@ url = 'http://tmdb.lewagon.com/movie/top_rated'
 movies = JSON.parse(URI.open(url).read)
 
 movies['results'].each do |movie|
-  Movie.create(title: movie['original_title'], overview: movie['overview'], rating: movie['vote_average'], poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}")
+  Movie.create(title: movie['original_title'], overview: movie['overview'], rating: movie['vote_average'], poster_url: "https://image.tmdb.org/t/p/original/#{movie['poster_path']}")
 end
